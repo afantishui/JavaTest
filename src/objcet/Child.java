e262d7643a9e59aa2b13b2f7f8e604e322b7dcef
afantishui <824144294@qq.com>
2019-07-24 07:22:00 +0800

    学习final

---
+++ src/objcet/Child.java
@@ -0,0 +1,14 @@
+package objcet;
+
+public class Child extends Parent {
+	
+	public static void main(String[] args) {
+		Parent a = new Parent();
+		a.sayhi();
+		Child b = new Child();
+		b.sayhi();
+	}
+
+
+
+}
---
+++ src/objcet/Parent.java
@@ -0,0 +1,8 @@
+package objcet;
+
+public class Parent {
+
+	public void sayhi() {
+		System.out.println("亲爱的观众朋友们，我想死你们了");
+	}
+}
