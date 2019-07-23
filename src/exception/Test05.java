d8dd14e7ced6ea2a590572f82b253279820f7c6a
afantishui <824144294@qq.com>
2019-07-24 07:58:55 +0800

    学习捕捉异常

---
+++ src/exception/Test05.java
@@ -0,0 +1,23 @@
+package exception;
+
+public class Test05 {
+
+	public Test05() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	public static void main(String[] args) {
+		// 空指针异常 NullPointerException
+		//Object obj = null;
+		//System.out.println(obj.hashCode());
+		
+		// 下标越界 ArrayIndexOutOfBoundsException
+		//int a[] = new int[6];
+		//System.out.println(a[10]);
+		
+		//计算错误 ArithmeticException
+		int b = 1/0;
+
+	}
+
+}
